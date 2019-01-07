2882b0e35081cac2513ca638864afa840e3f9da2
HBKarim2018 <44598175+HBKarim2018@users.noreply.github.com>
2019-01-07 11:59:37 +0200

    Update precache-manifest.9eef07021e411b95d1ea478738fdd4f8.js

--- precache-manifest.9eef07021e411b95d1ea478738fdd4f8.js
+++ precache-manifest.9eef07021e411b95d1ea478738fdd4f8.js
@@ -1,46 +1,46 @@
 self.__precacheManifest = [
   {
     "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/royate-react-app/static/media/fontawesome-webfont.af7ae505.woff2"
+    "url": "https://github.com/HBKarim2018/royate-app-react-website/tree/gh-pages/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
     "revision": "0ff15b9725a53d9a63dc",
-    "url": "/royate-react-app/static/css/main.a0ee74f8.chunk.css"
+    "url": "https://github.com/HBKarim2018/royate-app-react-website/tree/gh-pages/static/css/main.a0ee74f8.chunk.css"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
-    "url": "/royate-react-app/static/media/fontawesome-webfont.912ec66d.svg"
+    "url": "https://github.com/HBKarim2018/royate-app-react-website/tree/gh-pages/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
     "revision": "81fdf5c898918e49d9f2",
-    "url": "/royate-react-app/static/js/1.81fdf5c8.chunk.js"
+    "url": "https://github.com/HBKarim2018/royate-app-react-website/tree/gh-pages/static/js/1.81fdf5c8.chunk.js"
   },
   {
     "revision": "b165587bf516a3f5d1f4",
-    "url": "/royate-react-app/static/js/runtime~main.b165587b.js"
+    "url": "https://github.com/HBKarim2018/royate-app-react-website/tree/gh-pages/static/js/runtime~main.b165587b.js"
   },
   {
     "revision": "0ff15b9725a53d9a63dc",
-    "url": "/royate-react-app/static/js/main.0ff15b97.chunk.js"
+    "url": "https://github.com/HBKarim2018/royate-app-react-website/tree/gh-pages/static/js/main.0ff15b97.chunk.js"
   },
   {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/royate-react-app/static/media/fontawesome-webfont.674f50d2.eot"
+    "url": "https://github.com/HBKarim2018/royate-app-react-website/tree/gh-pages/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
-    "url": "/royate-react-app/static/media/fontawesome-webfont.fee66e71.woff"
+    "url": "https://github.com/HBKarim2018/royate-app-react-website/tree/gh-pages/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
     "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/royate-react-app/static/media/fontawesome-webfont.b06871f2.ttf"
+    "url": "https://github.com/HBKarim2018/royate-app-react-website/tree/gh-pages/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
     "revision": "81fdf5c898918e49d9f2",
-    "url": "/royate-react-app/static/css/1.e040fe30.chunk.css"
+    "url": "https://github.com/HBKarim2018/royate-app-react-website/tree/gh-pages/static/css/1.e040fe30.chunk.css"
   },
   {
     "revision": "9f9a20b5b9b157480c438036fa235575",
-    "url": "/royate-react-app/index.html"
+    "url": "https://github.com/HBKarim2018/royate-app-react-website/tree/gh-pages/index.html"
   }
 ];
