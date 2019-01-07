4204de5989384336a276f1d46249b303576b5d54
HBKarim2018 <44598175+HBKarim2018@users.noreply.github.com>
2019-01-07 12:03:03 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/royate-react-app/precache-manifest.9eef07021e411b95d1ea478738fdd4f8.js"
+  "/royate-app-react-website/precache-manifest.9eef07021e411b95d1ea478738fdd4f8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/royate-react-app/index.html", {
+workbox.routing.registerNavigationRoute("/royate-app-react-website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
